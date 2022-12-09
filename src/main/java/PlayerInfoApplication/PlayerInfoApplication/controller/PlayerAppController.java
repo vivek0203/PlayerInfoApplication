aad78aa632994d280fb2712ef5fb52287fc0e720
vivek0203 <111360797+vivek0203@users.noreply.github.com>
2022-12-09 13:01:08 +0530

    First Commi

--- src/main/java/PlayerInfoApplication/PlayerInfoApplication/controller/PlayerAppController.java
+++ src/main/java/PlayerInfoApplication/PlayerInfoApplication/controller/PlayerAppController.java
@@ -18,7 +18,7 @@ public class PlayerAppController {
         PlayerAppController.playerAppDao = playerAppDao;
     }
 
-    @GetMapping("/player_career_info/{name}")
+    @GetMapping("/player_career_info")
     public static Map<String, Object> GetPlayerCareerInfo(@RequestParam String player_name) throws SQLException {
         return playerAppDao.fetchPlayerCareerData(player_name);
     }
@@ -26,32 +26,32 @@ public class PlayerAppController {
     public static JSONObject GetFirst10PlayerData() throws SQLException{
         return playerAppDao.get10PlayerData();
     }
-    @GetMapping("/player_t20i_info/{name}")
+    @GetMapping("/player_t20i_info")
     public static Map<String, Object> fetchPlayerT20iInfoByName(@RequestParam String  player_name) throws SQLException {
         return playerAppDao.fetchPlayerT20iInfoByName(player_name);
     }
-    @GetMapping("/players_odi_info/{name}")
+    @GetMapping("/players_odi_info")
     public static Map<String, Object> fetchPlayerODIInfoByName(@RequestParam String  player_name) throws SQLException {
       return playerAppDao.fetchPlayerODIInfoByName(player_name);
     }
-    @GetMapping("/players_test_info/{name}")
+    @GetMapping("/players_test_info")
     public static Map<String, Object> fetchPlayerTestInfoByName(@RequestParam String  player_name) throws SQLException {
         return playerAppDao.fetchPlayerTestInfoByName(player_name);
     }
-    @GetMapping("/players_personal_info/{name}")
+    @GetMapping("/players_personal_info")
     public static Map<String, Object> fetchPlayerPersonalInfoByName(@RequestParam String  player_name) throws SQLException {
         return playerAppDao.fetchPlayerPersonalDataByName(player_name);
     }
 
-    @GetMapping("/top_5_experienced_players_data/{gender}")
+    @GetMapping("/top_5_experienced_players_data")
     public static Map<String, Object> fetchTop5ExperiencedPlayerDataByGender(@RequestParam String gender) throws SQLException {
         return playerAppDao.fetch5PlayerDataByGender(gender);
     }
-    @GetMapping("/top_5_century_scorer_info/{gender}")
+    @GetMapping("/top_5_century_scorer_info")
     public static Map<String, Object> fetchTop5CenturyScorerInfoByGender(@RequestParam String gender) throws SQLException {
         return playerAppDao.fetch5PlayerInfoByGender(gender);
     }
-    @GetMapping("/top_3_ODI_wicket_takers_info/{gender}")
+    @GetMapping("/top_3_ODI_wicket_takers_info")
     public static Map<String, Object> fetchTop3ODIWicketTakersInfoByGender(@RequestParam String gender) throws SQLException {
         return playerAppDao.fetch3PlayerInfoByGender(gender);
     }
